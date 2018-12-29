48a2162e9a570081f911f6595052e153a0e4aa51
a4711 <d4711@gmx.de>
2018-12-29 12:53:27 +0100

    some experiments

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,25 @@
 CRGB leds[1];
 
 void setup() {
-  Serial.begin(74880);
+  Serial.begin(74880); 
   FastLED.addLeds<WS2812B, 5, GRB>(leds, 1);
 }
 
 uint32_t counter = 0;
 
+uint32_t lastTick = 0;
+
 void loop() {
   leds[0].setHSV(counter++, 255, 255);
   FastLED.show();
   delay(10);
+
+
+  uint32_t tseconds = millis() / 1000;
+  if (tseconds > lastTick)
+  {
+    // every second:
+    Serial.println(ESP.getFlashChipSize());
+    lastTick = tseconds;
+  }
 }
