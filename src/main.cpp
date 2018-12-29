2c2a00853da6160b0bfad945b6bf824ffe653f1f
a4711 <d4711@gmx.de>
2018-12-29 12:21:22 +0100

    added WS2812B LED

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
 
+#define FASTLED_ESP8266_RAW_PIN_ORDER
+#include <FastLED.h>
+
+CRGB leds[1];
+
 void setup() {
   Serial.begin(74880);
+  FastLED.addLeds<WS2812B, 5, GRB>(leds, 1);
 }
 
+uint32_t counter = 0;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  leds[0].setHSV(counter++, 255, 255);
+  FastLED.show();
+  delay(10);
 }
